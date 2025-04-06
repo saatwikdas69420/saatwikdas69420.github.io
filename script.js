0df137e8e19c6569e148fe64307cf0b5912b1c7a
saatwikdas69420 <127622420+saatwikdas69420@users.noreply.github.com>
2025-04-06 16:09:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let website = "https://www.youtube.com"
-
 function trollLevel() {
-  let website = "https://cloud.onlinegames.io/games/2024/unity2/troll-level/index-og.html"
+  let website = "https://cloud.onlinegames.io/games/2024/unity2/troll-level/index-og.html";
   let iframe = document.getElementById("Iframe");
-  iframe.src = website;
+
+  if (iframe) {
+    iframe.src = website;
+  } else {
+    console.error("Iframe not found!");
+  }
 }
-document.addEventListener('DOMContentLoaded', setIframeSrc);
+
+document.addEventListener('DOMContentLoaded', trollLevel);

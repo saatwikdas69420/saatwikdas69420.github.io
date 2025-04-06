e35acf6a1f8a690dd629138b8d1fbdb8966bbebb
saatwikdas69420 <127622420+saatwikdas69420@users.noreply.github.com>
2025-04-06 16:21:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function trollLevel() {
   let website = "https://cloud.onlinegames.io/games/2024/unity2/troll-level/index-og.html";
-  let iframe = document.getElementById("Iframe");
+  let iframe = document.getElementById("iframe");
 
   if (iframe) {
     iframe.src = website;

7f144190e9b412bb9b851cceeab05d3b5235b357
saatwikdas69420 <127622420+saatwikdas69420@users.noreply.github.com>
2025-04-06 16:40:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function trollLevel() {
 }
 
 document.addEventListener('DOMContentLoaded', trollLevel);
+
+function cabybaraClickerPro() {
+  let website = "https://www.onlinegames.io/games/2023/q2/capybara-clicker-pro/index.html";
+  let iframe = document.getElementById("iframe");
+
+  if (iframe) {
+    iframe.src = website;
+  } else {
+    console.error("Iframe not found!");
+  }
+}
+
+document.addEventListener('DOMContentLoaded', capybaraClickerPro);
+
+function gtaSimulator() {
+  let website = "https://www.onlinegames.io/games/2023/unity2/gta-simulator/index.html";
+  let iframe = document.getElementById("iframe");
+
+  if (iframe) {
+    iframe.src = website;
+  } else {
+    console.error("Iframe not found!");
+  }
+}
+
+document.addEventListener('DOMContentLoaded', gtaSimulator);
